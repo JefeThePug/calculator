a8e3587e66f592f5b0153b7fe6f17369ea4f22ef
JefeThePug <rob.mihalko@gmail.com>
2022-08-28 14:35:01 +0900

    Update calc.py

--- calc.py
+++ calc.py
@@ -5,35 +5,40 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from functools import partial
 
+# COLORS
+C_BG = "black"
+C_FRAME = "black"
+C_DISP = "darkslategrey"
+C_DISP_TXT = "yellowgreen"
+C_BUT_NUM = "mistyrose"
+C_BUT_OP = "darkseagreen"
+C_BUT_FUNC = "palevioletred"
 
 class Win(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Caculator")
         self.setGeometry(200, 200, 550, 600)
-        self.setStyleSheet("background-color: black;")
-
+        self.setStyleSheet(f"background-color:{C_BG};")
         # FRAME
         self.fr_calc = QtWidgets.QFrame(self)
         self.fr_calc.setGeometry(QtCore.QRect(10, 10, 530, 580))
-        self.fr_calc.setStyleSheet("background-color: black;")
+        self.fr_calc.setStyleSheet(f"background-color:{C_FRAME};")
         # DISPLAY
-        self.lcd_out = QtWidgets.QLCDNumber(self.fr_calc)
-        self.lcd_out.setGeometry(QtCore.QRect(15, 15, 500, 70))
-        self.lcd_out.setStyleSheet(
-            "background-color: darkslategrey;color: yellowgreen;"
-        )
-        self.lcd_out.setSmallDecimalPoint(True)
-        self.lcd_out.setDigitCount(12)
+        self.lcd = QtWidgets.QLCDNumber(self.fr_calc)
+        self.lcd.setGeometry(QtCore.QRect(15, 15, 500, 70))
+        self.lcd.setStyleSheet(f"background-color:{C_DISP};color:{C_DISP_TXT};")
+        self.lcd.setSmallDecimalPoint(True)
+        self.lcd.setDigitCount(12)
         self.txt_mem = QtWidgets.QLabel(self.fr_calc)
         self.txt_mem.setGeometry(QtCore.QRect(17, 16, 16, 16))
-        self.txt_mem.setStyleSheet("background-color: 0;color: yellowgreen;")
+        self.txt_mem.setStyleSheet(f"background-color:0;color:{C_DISP_TXT};")
         # ROWS
         self.rows = []
         for num in range(5):
             r = QtWidgets.QSplitter(self.fr_calc)
             r.setGeometry(QtCore.QRect(15, 90 * num + 120, 498, 80))
-            r.setStyleSheet("QSplitter::handle { image: none; }")
+            r.setStyleSheet("QSplitter::handle{image:none;}")
             self.rows.append(r)
         # BUTTONS
         self.buttons = []
@@ -43,11 +48,7 @@ class Win(QMainWindow):
         self.clear()
 
     def fill_buttons(self):
-        styles = [
-            "background-color: darkseagreen;",
-            "background-color: mistyrose;",
-            "background-color: palevioletred;",
-        ]
+        style = [f"background-color:{x};" for x in [C_BUT_OP, C_BUT_NUM, C_BUT_FUNC]]
         text = [
             "AC", "MR", "MC", "M+", "CE",
              "7",  "8",  "9",  "±",  "√",
@@ -62,11 +63,8 @@ class Win(QMainWindow):
                 self.buttons.append(QtWidgets.QPushButton(self.rows[row]))
                 self.buttons[i].setText(txt)
                 self.buttons[i].setMaximumSize(QtCore.QSize(94, 80))
-                style = (txt[0] in "00123456789.AMC") + (txt[0] in ".AMC")
-                self.buttons[i].setStyleSheet(
-                    "padding: 15px 20px;font: bold 26px;text-align: center;"
-                    + styles[style]
-                )
+                s = (txt[0] in "00123456789.AMC") + (txt[0] in ".AMC")
+                self.buttons[i].setStyleSheet(f"font:bold 26px;text-align:center;{style[s]}")
                 self.buttons[i].clicked.connect(partial(self.clicked_button, txt))
 
     def clear(self, mem=False):
@@ -83,7 +81,8 @@ class Win(QMainWindow):
         self.txt_mem.setText("")
 
     def append_to_number(self, txt):
-        if txt == "." and "." in self.num_str: return
+        if txt == "." and "." in self.num_str:
+            return
 
         if self.finished_num or self.sq_push:
             self.finished_num = False
@@ -91,9 +90,9 @@ class Win(QMainWindow):
         if self.finished_eval:
             self.finished_eval = False
             self.eval_str = ""
-        
+
         self.num_str += txt
-        if (r := re.match(r"(0{1,2})(\d.*)", self.num_str)):
+        if (r := re.match(r"(0{1,2})(\d.*)", self.num_str)) :
             self.num_str = r.group(2)
 
     def add_operator(self, txt):
@@ -110,14 +109,17 @@ class Win(QMainWindow):
             self.eval_str += txt
         else:
             self.eval_str = self.eval_str[:-1] + txt
-        
+
         self.finished_num = True
 
     def totaling(self, txt):
         if txt == "%":
             self.num_str = f"{float(self.num_str)/100}"
 
-        if (s := self.eval_str + self.num_str).count(".") <= 1 and not re.findall(r"[-+*/]", s):
+        if not (
+            (s := self.eval_str + self.num_str).count(".") <= 1
+            and not re.findall(r"[-+*/]", s)
+        ):
             self.eval_str = s
         try:
             n = eval(self.eval_str)
@@ -137,11 +139,11 @@ class Win(QMainWindow):
             self.totaling(txt)
         elif txt == "√":
             self.sq_push = True
-            if "-" in self.num_str: 
+            if "-" in self.num_str:
                 self.num_str = "E"
             else:
                 self.num_str = f"{math.sqrt(float(self.num_str))}"
-                #self.finished_num = True
+                # self.finished_num = True
         elif txt == "±":
             n = float(self.num_str) if "." in self.num_str else int(self.num_str)
             self.num_str = f"{-n}"
@@ -162,13 +164,12 @@ class Win(QMainWindow):
     def clicked_button(self, txt):
         if "E" not in self.num_str or txt == "AC":
             self.check_clicked(txt)
-            if "E" in self.num_str: 
+            if "E" in self.num_str:
                 self.lcd_out.display("ERROR")
                 return
-            print(f"{self.num_str=} {self.eval_str=} {self.finished_num=} {self.finished_eval=}")
-            self.lcd_out.display(float(self.num_str) if self.num_str else "0")
+            self.lcd.display(float(self.num_str) if self.num_str else "0")
         else:
-            self.lcd_out.display("ERROR")
+            self.lcd.display("ERROR")
 
 
 if __name__ == "__main__":
